5c11865fd18e5a107a63c3c896d9e4281190e58b
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2025-08-01 18:28:14 +0300

    added httpx

--- utils/added_funcs.py
+++ utils/added_funcs.py
@@ -1,17 +1,36 @@
 # Добавление примечание к сделке в AmoCRM
+import httpx
 import requests
 
 from utils.get_grom_db import get_settings_string
 
 
 async def add_note_to_deal(deal_id, text):
-    settings_string = await get_settings_string()
+    try:
+        settings_string = await get_settings_string()
+        url = f'{settings_string.amo_crm_link}/api/v4/leads/{deal_id}/notes'
+        headers = {
+            'Authorization': f'Bearer {settings_string.amo_crm_token}',
+            'Content-Type': 'application/json'
+        }
+        data = [{"note_type": "common", "params": {"text": text}}]
 
-    url = f'{settings_string.amo_crm_link}/api/v4/leads/{deal_id}/notes'
-    headers = {'Authorization': f'Bearer {settings_string.amo_crm_token}', 'Content-Type': 'application/json'}
-    data = [{"note_type": "common", "params": {"text": text}}]
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code != 200:
-        print(f"❌Ошибка при добавлении примечания: {response.text}")
-    else:
-        print("✅Примечание успешно добавлено")
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, headers=headers, json=data)
+
+        if response.status_code != 200:
+            print(f"❌ Ошибка при добавлении примечания: {response.status_code} - {response.text}", flush=True)
+            return False
+        else:
+            print("✅ Примечание успешно добавлено", flush=True)
+            return True
+
+    except httpx.HTTPStatusError as e:
+        print(f"❌ HTTP-ошибка при добавлении примечания: {e}", flush=True)
+        return False
+    except httpx.RequestError as e:
+        print(f"❌ Ошибка сети при добавлении примечания: {e}", flush=True)
+        return False
+    except Exception as e:
+        print(f"❌ Неизвестная ошибка при добавлении примечания: {e}", flush=True)
+        return False
